59d822fb49c83f48274ce14f58ab82323fc5f088
CircleCI <eng-ops@pendo.io>
2023-02-27 13:50:29 +0000

    Release version 2.20.0

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Pendo",
-            url: "https://software.mobile.pendo.io/artifactory/ios-sdk-release/swiftui/2.20.0.6885/pendo-ios-sdk-xcframework.2.20.0.6885.zip",
-            checksum: "9a39f5f3753f8252974996854207fa963f42fd147e431409144d67fce3a1611b"
+            url: "https://software.mobile.pendo.io/artifactory/ios-sdk-release/swiftui/2.20.0.6901/pendo-ios-sdk-xcframework.2.20.0.6901.zip",
+            checksum: "396c2924a0f12175d624c17d3270529cca3df5367b1bb03723ee67a68a939226"
         ),
     ]
 )
