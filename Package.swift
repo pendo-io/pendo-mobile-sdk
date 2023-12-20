e9ecd7ced45ae17af7a2514ef26e6603ab6cd55a
Michael Rozenblat <michael.rozenblat@pendo.io>
2023-12-20 17:44:19 -0500

    APP-94422 remove empty line

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-​
+
 let package = Package(
     name: "Pendo",
     platforms: [
