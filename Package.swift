7554a4b0ce1b852a26f66b21d15053286cd58af3
Omri <56236821+0xkuj@users.noreply.github.com>
2024-09-30 09:55:15 +0300

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Pendo",
-            url: "https://software.mobile.pendo.io/artifactory/ios-sdk-release/3.4.1.8769/pendo-ios-sdk-xcframework.3.4.1.8769.zip",
-            checksum: "4a6000708249a7adc8accfda7388af55d222014ceeac7dd6a4ece5f480d47bdd"
+            url: "{{ SOURCE }}",
+            checksum: "{{ CHECKSUM }}"
         ),
     ]
 )
