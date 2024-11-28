a66fc2ce7ccf0ed4ecfcaf5c89b946fe2983c6b2
Omri Kugman <omri.kugman@pendo.io>
2024-11-28 12:35:49 +0200

    Release version 3.4.4

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Pendo",
-            url: "https://software.mobile.pendo.io/artifactory/ios-sdk-release/3.4.3.8833/pendo-ios-sdk-xcframework.3.4.3.8833.zip",
-            checksum: "2caf565f3e519deaea6cf9a80d377ff2b2c0c62cc31f459086890a55091203c9"
+            url: "https://software.mobile.pendo.io/artifactory/ios-sdk-release/3.4.4.8910/pendo-ios-sdk-xcframework.3.4.4.8910.zip",
+            checksum: "fa8c9736fad4714f672c29d24ad87e19ac046b04aea26ffc208667a8bb4f3f8d"
         ),
     ]
 )
