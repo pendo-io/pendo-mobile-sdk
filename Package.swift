8d656ffb71a271cdbbf7040d20c0cb35a9f6a108
Oren Dayan <oren.dayan77@gmail.com>
2025-03-26 11:58:26 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Pendo",
-            url: "https://software.mobile.pendo.io/artifactory/ios-sdk-release/3.6.1.9043/pendo-ios-sdk-xcframework.3.6.1.9043.zip",
-            checksum: "ca50f00e1e80fa15c45f18883b90e41936b4768f9d8a2268d3323a2b5edcbd24"
+            url: "https://software.mobile.pendo.io/artifactory/ios-sdk-release/3.6.2.9095/pendo-ios-sdk-xcframework.3.6.2.9095.zip",
+            checksum: "3d47bc04f1775454c66433a8967084f82bf0b7ae5adfcf11c6338de776767b1e"
         ),
     ]
 )
